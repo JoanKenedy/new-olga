0a8d7604f1bc8fca94185003b1765ace3d3f9388
JoanKenedy <joankenedi9311@gmail.com>
2024-04-18 19:34:56 -0600

    c

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,38 +2,53 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./hero.css";
 
 const Hero = () => {
   var settings = {
-    dots: true,
     infinite: true,
     fade: true,
-    speed: 500,
+    autoplaySpeed: 4000,
+    autoplay: true,
     slidesToShow: 1,
     slidesToScroll: 1,
   };
   return (
     <>
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
+      <div className="hero">
+        <Slider {...settings}>
+          <div className="slider">
+            <img
+              src="./src/assets/swiper-1.jpg"
+              alt=""
+              className="slider-img"
+            />
+            <div className="text-slider">
+              <h4>La mejor clinica materno fetal</h4>
+            </div>
+          </div>
+          <div className="slider">
+            <img
+              src="./src/assets/swiper-2.png"
+              alt=""
+              className="slider-img"
+            />
+            <div className="text-slider">
+              <h4>La mejor clinica materno fetal</h4>
+            </div>
+          </div>
+          <div className="slider">
+            <img
+              src="./src/assets/swiper-3.jpg"
+              alt=""
+              className="slider-img"
+            />
+            <div className="text-slider">
+              <h4>La mejor clinica materno fetal</h4>
+            </div>
+          </div>
+        </Slider>
+      </div>
     </>
   );
 };
